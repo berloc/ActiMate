be2ea1c0ab207039b512b446e2b314e8e2dfbc94
berloc <berloc1987@gmail.com>
2017-09-01 16:24:18 +0200

    initial commit

---
+++ backend/src/main/java/com/codecool/fittinder/FitTinderApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.fittinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTinderApplication.class, args);
+	}
+}
