33d5e84af0cfbdb3d9866cfbcd19854fa0ff3c35
berloc <berloc1987@gmail.com>
2017-09-03 10:45:34 +0200

    create securityConfig and allow to call route /api/** without authentication

---
+++ backend/src/main/java/com/codecool/fittinder/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.codecool.fittinder.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers("/api/**").permitAll();
+    }
+}
