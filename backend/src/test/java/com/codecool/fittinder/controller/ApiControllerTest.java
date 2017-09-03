b454468166ba6383bacdb1348b700b29ea0e2bb0
berloc <berloc1987@gmail.com>
2017-09-03 12:16:58 +0200

    add statusTestReturnJSON method

---
+++ backend/src/test/java/com/codecool/fittinder/controller/ApiControllerTest.java
@@ -0,0 +1,24 @@
+package com.codecool.fittinder.controller;
+
+import com.codecool.fittinder.config.TestConfig;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+public class ApiControllerTest extends TestConfig {
+
+
+    @Before
+    public void setup() {
+        initMockMvc();
+    }
+
+    @Test
+    public void statusTestReturnJSON() throws Exception {
+        mockMvc.perform(get("/api/status"))
+                .andExpect(content().string(containsString("\"status\":\"server is running\"")));
+    }
+}
