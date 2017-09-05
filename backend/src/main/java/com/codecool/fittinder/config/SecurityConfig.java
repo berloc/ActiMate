f286a82c3d9a3eda7ab1de8920faf34396ee2adc
berloc <berloc1987@gmail.com>
2017-09-05 22:41:01 +0200

    add login and logout settings to security config

--- backend/src/main/java/com/codecool/fittinder/config/SecurityConfig.java
+++ backend/src/main/java/com/codecool/fittinder/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,6 +34,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
             .antMatchers("/api/**", "/registration").permitAll()
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
+            .permitAll()
             .and()
         ;
     }
