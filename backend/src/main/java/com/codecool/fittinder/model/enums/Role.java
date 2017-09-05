b98e017598b7520a2d6ff6e9e7a68ceb330fcc78
berloc <berloc1987@gmail.com>
2017-09-05 13:42:01 +0200

    modify Role enum structure

--- backend/src/main/java/com/codecool/fittinder/model/enums/Role.java
+++ backend/src/main/java/com/codecool/fittinder/model/enums/Role.java
@@ -1,14 +1,9 @@
 package com.codecool.fittinder.model.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor @NoArgsConstructor
 public enum Role {
-    ADMIN("Admin"),
-    USER("User");
-
-    private String type;
+    ADMIN,
+    USER
 }
