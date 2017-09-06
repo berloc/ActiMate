cc66d01c249fbe96dd35e5562de1942af1646294
berloc <berloc1987@gmail.com>
2017-09-06 12:13:56 +0200

    add getInterest to controller

--- backend/src/main/java/com/codecool/fittinder/controller/ApiController.java
+++ backend/src/main/java/com/codecool/fittinder/controller/ApiController.java
@@ -1,15 +1,19 @@
 package com.codecool.fittinder.controller;
 
+import com.codecool.fittinder.model.Interest;
+import com.codecool.fittinder.repository.InterestRepository;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -18,6 +22,9 @@ public class ApiController {
 
     private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
 
+    @Autowired
+    private InterestRepository interestRepository;
+
     @GetMapping(value = "/status")
     public String status(HttpServletRequest request) throws JSONException {
         logger.debug("{} is called with method: {}", request.getRequestURI(), request.getMethod());
@@ -25,4 +32,9 @@ public class ApiController {
         return status.toString();
     }
 
+    @GetMapping(value = "/interest")
+    public List<Interest> getInterest(HttpServletRequest request) {
+        logger.debug("{} is called with method: {}", request.getRequestURI(), request.getMethod());
+        return interestRepository.findAll();
+    }
 }
