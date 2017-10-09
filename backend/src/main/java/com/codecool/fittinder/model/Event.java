1be86488e7e3f2971228350002f1158ad4048f82
berloc <berloc1987@gmail.com>
2017-10-09 13:54:38 +0200

    please

--- backend/src/main/java/com/codecool/fittinder/model/Event.java
+++ backend/src/main/java/com/codecool/fittinder/model/Event.java
@@ -1,5 +1,6 @@
 package com.codecool.fittinder.model;
 
+import com.codecool.fittinder.model.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,13 +17,30 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @OneToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
     private String name;
     private String location;
     private Integer participants;
     private Date startDate;
     @Column(columnDefinition = "TEXT")
     private String description;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "interest", referencedColumnName = "name")
     private Interest interest;
+    private Status status;
+    private Boolean applied;
+
+    public Event(User user, String name, String location, Integer participants, Date startDate, String description, Interest interest, Status status) {
+        this.user = user;
+        this.name = name;
+        this.location = location;
+        this.participants = participants;
+        this.startDate = startDate;
+        this.description = description;
+        this.interest = interest;
+        this.status = status;
+        this.applied = false;
+    }
 }
