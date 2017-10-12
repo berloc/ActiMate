89736d65cc27c51ba52e71878f1a726a93e8cc53
berloc <berloc1987@gmail.com>
2017-10-12 10:54:07 +0200

    modify to static the final status field

--- backend/src/main/java/com/codecool/fittinder/controller/UserController.java
+++ backend/src/main/java/com/codecool/fittinder/controller/UserController.java
@@ -20,7 +20,7 @@ import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
-public class UserController extends AbstractController{
+public class UserController extends AbstractController {
 
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -29,13 +29,13 @@ public class UserController extends AbstractController{
     UserDetailsService userDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-    private final String status = "status";
+    private static final String STATUS = "status";
 
     @PostMapping(value = "/registration")
     @ResponseBody
     public String registration(@RequestBody @Valid UserDto userDto, HttpServletRequest request) throws JSONException, MessagingException, EmailIsInTheDatabaseException {
         logger.debug(debugLogMes, request.getRequestURI(), request.getMethod());
-        JSONObject response = new JSONObject().put(status,"fail");
+        JSONObject response = new JSONObject().put(STATUS, "fail");
         User user = converter.convertToUser(userDto);
         if (userService.findByUsername(user.getUsername()) == null) {
             userService.createUser(user);
@@ -43,10 +43,10 @@ public class UserController extends AbstractController{
             profileRepository.save(profile);
             logger.info("Save profile into the {} table", profile.getClass().getSimpleName());
 //            emailService.sendMessage(user.getUsername(), "registration", template.getText());
-            response.put(status,"success");
+            response.put(STATUS, "success");
         } else {
             logger.debug("{} is already in the db", user.getUsername());
-            response.put(status,"username exist");
+            response.put(STATUS, "username exist");
             throw new EmailIsInTheDatabaseException();
         }
 
@@ -56,18 +56,18 @@ public class UserController extends AbstractController{
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private String validationException(MethodArgumentNotValidException e) throws JSONException {
         logger.info("{} occurred not valid username or password format while tried to save the User into the db, {} ", e.getClass().getSimpleName(), e.getMessage());
-        return new JSONObject().put(status, "invalid username or password format").toString();
+        return new JSONObject().put(STATUS, "invalid username or password format").toString();
     }
 
     @ExceptionHandler(MessagingException.class)
     private String messagingException(MessagingException e) throws JSONException {
         logger.info("{} occurred while trying to send the welcome username; {} ", e.getClass().getSimpleName(), e.getMessage());
-        return new JSONObject().put(status, "messaging exception").toString();
+        return new JSONObject().put(STATUS, "messaging exception").toString();
     }
 
     @ExceptionHandler(EmailIsInTheDatabaseException.class)
     private String emailIsInTheDatabase(EmailIsInTheDatabaseException e) throws JSONException {
         logger.info("{} occurred while trying to save the user into the db; {} ", e.getClass().getSimpleName(), e.getMessage());
-        return new JSONObject().put(status, "username exist").toString();
+        return new JSONObject().put(STATUS, "username exist").toString();
     }
 }
